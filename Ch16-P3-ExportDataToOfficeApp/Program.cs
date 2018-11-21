9bafa664d48ed18486f7fc553587340cc499878e
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-11-21 17:17:22 +0500

    git ignore added with a little code update

--- Ch16-P3-ExportDataToOfficeApp/Program.cs
+++ Ch16-P3-ExportDataToOfficeApp/Program.cs
@@ -4,12 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
 namespace Ch16_P3_ExportDataToOfficeApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<Car> carsInStock = new List<Car>
+            {
+                new Car {Color="Green", Make="VW", PetName="Mary"},
+                new Car {Color="Red", Make="Saab", PetName="Mel"},
+                new Car {Color="Black", Make="Ford", PetName="Hank"},
+                new Car {Color="Yellow", Make="BMW", PetName="Davie"}
+            };
+
+        }
+
+        static void ExportToExcel(List<Car> carsInStock)
+        {
+            //
+        }
         }
     }
-}
